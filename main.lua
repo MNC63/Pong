cf11ae4ef8ad0b060c89d63265faaeb502ce6933
MNC63 <nloc8380@gmail.com>
2025-06-14 18:25:25 +0700

    Lua and Love2D

---
+++ main.lua
@@ -0,0 +1,29 @@
+push = require'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT ,WINDOW_WIDTH, WINDOW_HEIGHT, {
+        resizable = false,
+        vsync = true,
+        fullscreen = false
+    })
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:apply('start')
+    love.graphics.printf("hello, Pong!", 0, VIRTUAL_HEIGHT /2 - 6, VIRTUAL_WIDTH, 'center')
+    push:apply('end')
+end
